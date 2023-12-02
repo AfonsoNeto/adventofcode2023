197491f6b1c3701518a276005c7377359da05657
Afonso Neto <afonso.pontesneto@gmail.com>
2023-12-02 19:49:44 +0000

    [day01 - part1] Fix file execution output

--- day01/part1/solution.rb
+++ day01/part1/solution.rb
@@ -1,6 +1,8 @@
 file_lines = File.readlines('../input.txt')
 
-file_lines.map do |line|
+total = file_lines.map do |line|
   matching_numbers = line.scan(/\d{1}/)
   (matching_numbers.first + matching_numbers.last).to_i
 end.sum
+
+puts total
